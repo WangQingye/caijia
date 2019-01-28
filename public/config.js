65ce57958de69372ae473d08ce4adb08e63dcd67
wangqingye <wangqingye@cetcxl.com>
2019-01-28 17:53:43 +0800

    reset config

--- public/config.js
+++ public/config.js
@@ -1,3 +1,2 @@
 window.userUrl = 'http://172.16.5.14/zp_user_manager'
-// window.productUrl = 'http://172.16.5.14/zp_product_manager'
-window.productUrl = 'http://172.16.0.87:8081'
+window.productUrl = 'http://172.16.5.14/zp_product_manager'
