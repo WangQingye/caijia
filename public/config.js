0b645475b84514c026c213bccb45d12ef6994ca7
wangqingye <wangqingye@cetcxl.com>
2019-01-30 17:22:09 +0800

    test dev

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.userUrl = 'http://172.16.5.14/zp_user_manager'
-window.productUrl = 'http://172.16.5.14/zp_product_manager'
+window.userUrl = 'http://zp.cetcxl.com/zp_user_manager'
+window.productUrl = 'http://zp.cetcxl.com/zp_product_manager'
