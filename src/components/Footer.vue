8c8ec146d5497f0aad6c30a77b50a6ac828636e6
绳圆圆 <shengyuanyuan@cetcxl.com>
2019-01-30 10:59:50 +0800

    适配分辨率

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -79,15 +79,15 @@ export default {
         },
         {
           name: "农产品投保",
-          path: "/"
+          path: ""
         },
         {
           name: "理赔公示",
-          path: "/"
+          path: ""
         },
         {
           name: "联系我们",
-          path: "/"
+          path: ""
         }
       ],
       friendsLinks: [
