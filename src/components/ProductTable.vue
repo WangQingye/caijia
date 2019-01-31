fbc5c606277f7e5db3c6712cf4a37fc1a4ff7722
wangqingye <wangqingye@cetcxl.com>
2019-01-31 09:40:53 +0800

    fix render time

--- src/components/ProductTable.vue
+++ src/components/ProductTable.vue
@@ -1,166 +1,174 @@
 <template>
-    <div class="table-box">
-        <el-row :gutter="20" class="table-row">
-            <el-col :span="4" class="table-dot">
-                <img class="dot" src="@/assets/imgs/yuan2.png">
-            </el-col>
-            <el-col :span="16" >
-                <p class="time">{{renderTime(listData[productIndex].ocreateTime)}}</p>
-                <!-- <p class="details">{{listData[productIndex].oremark}}</p> -->
-                <p class="details">{{ listData[productIndex].stepOrde==3 ? '标签申请': listData[productIndex].oremark}}</p>
-            </el-col>
-            <el-col :span="4" class="goodsImg" style="padding:0">
-                <!-- <img class="goods" src='@/assets/imgs/goods.jpg'> -->
-                 <img class="goods" :src="imgs[listData[productIndex].stepOrde-1]">
-            </el-col>
-        </el-row>
-    </div>
+  <div class="table-box">
+    <el-row
+      :gutter="20"
+      class="table-row"
+    >
+      <el-col
+        :span="4"
+        class="table-dot"
+      >
+        <img
+          class="dot"
+          src="@/assets/imgs/yuan2.png"
+        >
+      </el-col>
+      <el-col :span="16">
+        <p class="time">{{renderTime(listData[productIndex].ocreateTime)}}</p>
+        <!-- <p class="details">{{listData[productIndex].oremark}}</p> -->
+        <p class="details">{{ listData[productIndex].stepOrde==3 ? '标签申请': listData[productIndex].oremark}}</p>
+      </el-col>
+      <el-col
+        :span="4"
+        class="goodsImg"
+        style="padding:0"
+      >
+        <!-- <img class="goods" src='@/assets/imgs/goods.jpg'> -->
+        <img
+          class="goods"
+          :src="imgs[listData[productIndex].stepOrde-1]"
+        >
+      </el-col>
+    </el-row>
+  </div>
 
 </template>
 <script>
-import step1 from '@/assets/imgs/1.png'
-import step2 from '@/assets/imgs/2.png'
-import step3 from '@/assets/imgs/3.png'
-import step4 from '@/assets/imgs/4.png'
-import step5 from '@/assets/imgs/5.png'
-export default{
-    data(){
-        return {
-            imgs:[
-                step1,step2,step3,step4,step5
-            ]
-        }
+import step1 from "@/assets/imgs/1.png";
+import step2 from "@/assets/imgs/2.png";
+import step3 from "@/assets/imgs/3.png";
+import step4 from "@/assets/imgs/4.png";
+import step5 from "@/assets/imgs/5.png";
+export default {
+  data() {
+    return {
+      imgs: [step1, step2, step3, step4, step5]
+    };
+  },
+  props: {
+    listData: {
+      type: Array,
+      default: () => {
+        return [
+          {
+            ocreateTime: "时间",
+            oremark: "产品描述",
+            stepOrde: ""
+            // imagineSrc:goods
+          }
+        ];
+      }
     },
-    props:{
-        listData:{
-            type:Array,
-            default:()=>{
-                return[
-                    {
-                        ocreateTime:'时间',
-                        oremark:'产品描述',
-                        stepOrde:''
-                        // imagineSrc:goods
-                    }
-                ];
-            }
-        },
-        productIndex:{
-            default:()=>{
-                return []
-            }
-        }
-    },
-    mounted(){
-
-    },
-    methods:{
-         renderTime(date) {
-            let aa = date.replace(/\-/g,'/');
-            let bb = aa.replace(/T/g,' ');
-            let cc = bb.split('.')[0];
-            let dataObj = new Date(cc);
-            let txt = '';
-            txt += dataObj.getFullYear() + '-';
-            txt += dataObj.getMonth() + 1 + '-';
-            txt += dataObj.getDate() + ' ';
-            txt += dataObj.getHours() + ':';
-            txt += dataObj.getMinutes() + ':';
-            txt += dataObj.getSeconds();
-            return txt;
-        }
-
+    productIndex: {
+      default: () => {
+        return [];
+      }
     }
-}
+  },
+  mounted() {},
+  methods: {
+    renderTime(date) {
+      let aa = date.replace(/\-/g, "/");
+      let bb = aa.replace(/T/g, " ");
+      let cc = bb.split(".")[0];
+      let dataObj = new Date(cc);
+      let txt = "";
+      txt += dataObj.getFullYear() + "-";
+      txt += this.PrefixInteger(dataObj.getMonth() + 1, 2) + "-";
+      txt += this.PrefixInteger(dataObj.getDate(), 2) + " ";
+      txt += this.PrefixInteger(dataObj.getHours(), 2) + ":";
+      txt += this.PrefixInteger(dataObj.getMinutes(), 2) + ":";
+      txt += this.PrefixInteger(dataObj.getSeconds(), 2);
+      return txt;
+    },
+    PrefixInteger(num, n) {
+      return (Array(n).join(0) + num).slice(-n);
+    }
+  }
+};
 </script>
 
 <style lang="scss">
+.table-box {
+  width: 311px;
+  height: 191px;
+  margin: 0 auto;
+  margin-top: -25px;
+  position: relative;
 
-.table-box{
-    width:311px;
-    height:191px;
-    margin:0 auto;
-    margin-top:-25px;
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+  .table-row {
     position: relative;
-
-    .el-row {
-        margin-bottom: 20px;
-        &:last-child {
-        margin-bottom: 0;
-        }
+  }
+  .table-dot {
+    position: relative;
+  }
+  .table-dot:after {
+    content: "";
+    display: inline-block;
+    width: 3px;
+    height: 146px;
+    float: left;
+    background: rgba(242, 242, 242, 1);
+    position: absolute;
+    top: 22px;
+    left: -4px;
+  }
+  .dot {
+    position: absolute;
+    left: -11px;
+  }
+  .time {
+    font-size: 18px;
+    font-family: MicrosoftYaHei-Bold;
+    font-weight: bold;
+    color: rgba(85, 85, 85, 1);
+    text-align: left;
+  }
+  .details {
+    margin-top: 15px;
+    font-size: 14px;
+    font-family: MicrosoftYaHei;
+    font-weight: 400;
+    color: rgba(117, 117, 117, 1);
+    line-height: 22px;
+    text-align: left;
+  }
+  .goodsImg {
+    position: relative;
+    .goods {
+      border-radius: 50%;
+      width: 70px;
+      height: 70px;
+      position: absolute;
+      top: 32px;
+      left: 24px;
     }
-    .el-col {
-        border-radius: 4px;
-
-    }
-    .bg-purple-dark {
-        background: #99a9bf;
-    }
-    .bg-purple {
-        background: #d3dce6;
-    }
-    .bg-purple-light {
-        background: #e5e9f2;
-    }
-    .grid-content {
-        border-radius: 4px;
-        min-height: 36px;
-    }
-    .row-bg {
-        padding: 10px 0;
-        background-color: #f9fafc;
-    }
-    .table-row{
-        position:relative;
-    }
-    .table-dot{
-        position: relative;
-    }
-    .table-dot:after{
-        content:"";
-        display:inline-block;
-        width:3px;
-        height:146px;
-        float:left;
-        background:rgba(242,242,242,1);
-        position:absolute;
-        top:22px;
-        left:-4px;
-    }
-    .dot{
-        position:absolute;
-        left:-11px;
-    }
-    .time{
-        font-size:18px;
-        font-family:MicrosoftYaHei-Bold;
-        font-weight:bold;
-        color:rgba(85,85,85,1);
-        text-align:left;
-    }
-    .details{
-        margin-top:15px;
-        font-size:14px;
-        font-family:MicrosoftYaHei;
-        font-weight:400;
-        color:rgba(117,117,117,1);
-        line-height:22px;
-        text-align:left;
-    }
-    .goodsImg{
-        position: relative;
-        .goods{
-            border-radius:50%;
-            width:70px;
-            height:70px;
-            position:absolute;
-            top:32px;
-            left:24px;
-        }
-
-    }
+  }
 }
-
-
-
 </style>
--- src/views/Phone.vue
+++ src/views/Phone.vue
@@ -1,74 +1,91 @@
 <template>
-    <div class="phone">
-      <img class="banner-wap" src="@/assets/imgs/banner-wap.png"  alt="">
-      <head-line :text="'原产地'" :text02="'COUNTRY   OF  ORIGIN'"></head-line>
-      <div class="map">
-        <img class="map-wap" src="@/assets/imgs/map-wap.png" >
-        <div class="location" >
-          <img src="@/assets/imgs/Location.png">
-          <p>四川</p>
+  <div class="phone">
+    <img
+      class="banner-wap"
+      src="@/assets/imgs/banner-wap.png"
+      alt=""
+    >
+    <head-line
+      :text="'原产地'"
+      :text02="'COUNTRY   OF  ORIGIN'"
+    ></head-line>
+    <div class="map">
+      <img
+        class="map-wap"
+        src="@/assets/imgs/map-wap.png"
+      >
+      <div class="location">
+        <img src="@/assets/imgs/Location.png">
+        <p>四川</p>
+      </div>
+    </div>
+    <head-line
+      :text="'产品信息'"
+      :text02="'PRODUCT INFORMATION'"
+    ></head-line>
+    <div class="product-msg">
+      <ul>
+        <li>
+          <span class="pro-title">产品编码</span>
+          <span class="pro-details">{{parseQRCode(urlParam)}}</span>
+        </li>
+        <li>
+          <span class="pro-title">产地</span>
+          <span class="pro-details">{{products.sorigin}}</span>
+        </li>
+        <li>
+          <span class="pro-title">农产种类</span>
+          <span class="pro-details">{{products.skindName}}</span>
+        </li>
+        <li>
+          <span class="pro-title">农产品种</span>
+          <span class="pro-details">{{products.svarietyName}}</span>
+        </li>
+        <li>
+          <span class="pro-title">采摘时间</span>
+          <span class="pro-details">{{renderTime(products.sstoreTime)}}</span>
+        </li>
+        <li>
+          <span class="pro-title">企业</span>
+          <span class="pro-details">{{products.ocompanyName}}</span>
+        </li>
+      </ul>
+    </div>
+    <head-line
+      :text="'产品溯源'"
+      :text02="'PRODUCT  TRACEABILITY'"
+    ></head-line>
+    <div class="product">
+      <div class="dot-row">
+        <div class="tableDot">
+          <img src="@/assets/imgs/yuan1.png">
         </div>
       </div>
-      <head-line :text="'产品信息'" :text02="'PRODUCT INFORMATION'"></head-line>
-      <div class="product-msg">
-        <ul>
-          <li>
-            <span class="pro-title">产品编码</span>
-            <span class="pro-details">{{parseQRCode(urlParam)}}</span>
-          </li>
-          <li>
-            <span class="pro-title">产地</span>
-            <span class="pro-details">{{products.sorigin}}</span>
-          </li>
-          <li>
-            <span class="pro-title">农产种类</span>
-            <span class="pro-details">{{products.skindName}}</span>
-          </li>
-          <li>
-            <span class="pro-title">农产品种</span>
-            <span class="pro-details">{{products.svarietyName}}</span>
-          </li>
-           <li>
-            <span class="pro-title">采摘时间</span>
-            <span class="pro-details">{{renderTime(products.sstoreTime)}}</span>
-          </li>
-          <li>
-            <span class="pro-title">企业</span>
-            <span class="pro-details">{{products.ocompanyName}}</span>
-          </li>
-        </ul>
-      </div>
-      <head-line :text="'产品溯源'" :text02="'PRODUCT  TRACEABILITY'"></head-line>
-      <div class="product">
-        <div  class="dot-row">
-          <div  class="tableDot">
-              <img src="@/assets/imgs/yuan1.png">
-          </div>
-        </div>
-        <product-table
+      <product-table
         :listData="productDetails"
         v-for="(item,index) in productDetails "
-        :key="index" :productIndex="index">
-        </product-table>
-      </div>
-      <up-to-now></up-to-now>
+        :key="index"
+        :productIndex="index"
+      >
+      </product-table>
     </div>
+    <up-to-now></up-to-now>
+  </div>
 </template>
 
 <script>
 import fetch from "@/assets/js/fetch.js";
-import HeadLine from '@/components/HeadLine.vue'
-import ProductTable from '@/components/ProductTable.vue'
-import UpToNow from '@/components/UpToNow.vue'
-
+import HeadLine from "@/components/HeadLine.vue";
+import ProductTable from "@/components/ProductTable.vue";
+import UpToNow from "@/components/UpToNow.vue";
 
 export default {
   name: "phone",
   data() {
     return {
-      urlParam:"",
-      products:[],
-      productDetails:[
+      urlParam: "",
+      products: [],
+      productDetails: [
         // {
         //       ocreateTime:'2018-12-02',
         //       oremark:'入库',
@@ -97,174 +114,174 @@ export default {
       ]
     };
   },
-  props: {
-  },
+  props: {},
   methods: {
-    async getProductData(){
+    async getProductData() {
       //console.log("当前参数：" + this.urlParam)
-        let data = await fetch(
-          '/queryController/queryOriginActionByBatch',
-          {
-           urlParam:this.urlParam
+      let data = await fetch("/queryController/queryOriginActionByBatch", {
+        urlParam: this.urlParam
+      });
+      console.log(data.data);
+      if (data.code == 0) {
+        //alert(data.data.length)
+        for (var i = 0; i < data.data.length; i++) {
+          var item = data.data[i];
+          console.log(item.stepOrde);
+          if (item.stepOrde == 1) {
+            //console.log("入库");
+            this.products = item;
           }
-        );
-        console.log(data.data)
-        if(data.code == 0){
-          //alert(data.data.length)
-          for(var i=0;i<data.data.length;i++){
-            var item=data.data[i];
-            console.log(item.stepOrde)
-            if(item.stepOrde==1){
-              //console.log("入库");
-              this.products=item
-            }
-            // else if(item.stepOrde==2){
-            //   //console.log("检测")
-            // }else if(item.stepOrde==3){
-            //   //console.log("标签申请")
-            // }else if(item.stepOrde==4){
-            //   //console.log("出库")
-            // }else{
-            //   //console.log("物流")
-            // }
-          }
-          //alert(this.products)
-        this.productDetails=data.data
+          // else if(item.stepOrde==2){
+          //   //console.log("检测")
+          // }else if(item.stepOrde==3){
+          //   //console.log("标签申请")
+          // }else if(item.stepOrde==4){
+          //   //console.log("出库")
+          // }else{
+          //   //console.log("物流")
+          // }
         }
-
+        //alert(this.products)
+        this.productDetails = data.data;
+      }
     },
     renderTime(date) {
-      var dateee = new Date(date).toJSON();
-      return new Date(+new Date(dateee) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '')
+      let aa = date.replace(/\-/g, "/");
+      let bb = aa.replace(/T/g, " ");
+      let cc = bb.split(".")[0];
+      let dataObj = new Date(cc);
+      let txt = "";
+      txt += dataObj.getFullYear() + "-";
+      txt += this.PrefixInteger(dataObj.getMonth() + 1, 2) + "-";
+      txt += this.PrefixInteger(dataObj.getDate(), 2) + " ";
+      txt += this.PrefixInteger(dataObj.getHours(), 2) + ":";
+      txt += this.PrefixInteger(dataObj.getMinutes(), 2) + ":";
+      txt += this.PrefixInteger(dataObj.getSeconds(), 2);
+      return txt;
     },
-    parseQRCode(codeURL){
+    PrefixInteger(num, n) {
+      return (Array(n).join(0) + num).slice(-n);
+    },
+    parseQRCode(codeURL) {
       var code = codeURL.split("id%3D");
       return code[1];
     },
-    getUrlParam(){
-      this.urlParam=location.href;
-      var num=this.urlParam.indexOf("?");
-      this.urlParam=this.urlParam.substr(num+10)
+    getUrlParam() {
+      this.urlParam = location.href;
+      var num = this.urlParam.indexOf("?");
+      this.urlParam = this.urlParam.substr(num + 10);
       //alert(this.urlParam)
     }
   },
-  mounted(){
+  mounted() {
     this.getUrlParam();
     this.getProductData();
-
   },
-  watch: {
-
-  },
+  watch: {},
   components: {
     HeadLine,
     ProductTable,
     UpToNow
   }
-}
+};
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-
 .phone {
+  width: 100%;
+  margin: 0 auto;
+  .banner-wap {
     width: 100%;
     margin: 0 auto;
-    .banner-wap{
-      width:100%;
-      margin:0 auto;
-    }
-    .map{
-      width:100%;
-      margin:0 auto;
-      position:relative;
-      .map-wap{
-        width:100%;
-        margin:0 auto;
-      }
-      .location{
-        position:absolute;
-        top:69px;
-        left:95px;
-        img{
-          width:50%;
-        }
-          p{
-          font-size:10px;
-          font-family:MicrosoftYaHei;
-          font-weight:400;
-          color:rgba(85,85,85,1);
-          line-height:20px;
-        }
-      }
-
   }
-  .product-msg{
-    width:100%;
-    margin:0 auto;
-    border:1px solid rgba(242, 242, 242, 1);
-    ul{
-      li{
-        width:100%;
-        height:42px;
-        border-bottom:1px solid rgba(242, 242, 242, 1);
-        position:relative;
-        span{
-          line-height:45px;
+  .map {
+    width: 100%;
+    margin: 0 auto;
+    position: relative;
+    .map-wap {
+      width: 100%;
+      margin: 0 auto;
+    }
+    .location {
+      position: absolute;
+      top: 69px;
+      left: 95px;
+      img {
+        width: 50%;
+      }
+      p {
+        font-size: 10px;
+        font-family: MicrosoftYaHei;
+        font-weight: 400;
+        color: rgba(85, 85, 85, 1);
+        line-height: 20px;
+      }
+    }
+  }
+  .product-msg {
+    width: 100%;
+    margin: 0 auto;
+    border: 1px solid rgba(242, 242, 242, 1);
+    ul {
+      li {
+        width: 100%;
+        height: 42px;
+        border-bottom: 1px solid rgba(242, 242, 242, 1);
+        position: relative;
+        span {
+          line-height: 45px;
         }
-        span.pro-title{
-          font-size:14px;
-          font-family:MicrosoftYaHei-Bold;
-          font-weight:bold;
-          color:rgba(85,85,85,1);
-          line-height:42px;
-          position:absolute;
-          left:55px;
+        span.pro-title {
+          font-size: 14px;
+          font-family: MicrosoftYaHei-Bold;
+          font-weight: bold;
+          color: rgba(85, 85, 85, 1);
+          line-height: 42px;
+          position: absolute;
+          left: 55px;
         }
-        span.pro-details{
-          position:absolute;
-          left:140px;
-          font-size:14px;
-          font-family:ArialMT;
-          font-weight:400;
-          color:rgba(117,117,117,1);
-          line-height:42px;
+        span.pro-details {
+          position: absolute;
+          left: 140px;
+          font-size: 14px;
+          font-family: ArialMT;
+          font-weight: 400;
+          color: rgba(117, 117, 117, 1);
+          line-height: 42px;
         }
       }
     }
   }
-  .product{
-    width:100%;
-    margin:0 auto;
-    padding-top:30px;
-     position: relative;
+  .product {
+    width: 100%;
+    margin: 0 auto;
+    padding-top: 30px;
+    position: relative;
   }
-  .dot-row{
+  .dot-row {
     width: 311px;
     height: 29px;
     margin: 0 auto;
     position: relative;
-    .tableDot{
-
-      img{
+    .tableDot {
+      img {
         position: absolute;
-        top:-65px;
-        left:-20px;
+        top: -65px;
+        left: -20px;
       }
     }
 
-    .tableDot:after{
-        content:"";
-        display:inline-block;
-        width:3px;
-        height:49px;
-        float: left;
-        background:rgba(242,242,242,1);
-        position:absolute;
-        top:-49px;
-        left:-14px;
+    .tableDot:after {
+      content: "";
+      display: inline-block;
+      width: 3px;
+      height: 49px;
+      float: left;
+      background: rgba(242, 242, 242, 1);
+      position: absolute;
+      top: -49px;
+      left: -14px;
     }
   }
-
 }
-
 </style>
