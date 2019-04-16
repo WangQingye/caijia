b64e40edb8549f80648883608f9d0fba79d6339e
绳圆圆 <shengyuanyuan@cetcxl.com>
2019-04-16 13:58:27 +0800

    签名

--- public/config.js
+++ public/config.js
@@ -1,2 +1,5 @@
-window.userUrl = 'http://172.16.5.13/zp_user_manager'
-window.productUrl = 'http://172.16.5.13/zp_product_manager'
+// window.userUrl = 'http://172.16.5.13/zp_user_manager'
+// window.productUrl = 'http://172.16.5.13/zp_product_manager'
+
+window.userUrl = 'http://172.16.0.87:8080'
+window.productUrl = 'http://172.16.0.87:8070'
